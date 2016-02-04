ab4b24ae0eb61d7eefb182f199254e97666839b7
Haritha Yelamanchili <haritha.manchili@gmail.com>
2016-02-04 15:56:18 -0600

    Create mainclass.java

---
+++ mainclass.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+public class barla {
+	public static void main(String r[]){
+		welcomepage wp=new welcomepage();
+		wp.setVisible(true);
+		wp.setBounds(500,300,500,400);
+		wp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
